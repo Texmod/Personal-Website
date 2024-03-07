c7493dc1e1b3014f97127e57a25311574c346f85
Texmod <162580160+Texmod@users.noreply.github.com>
2024-03-07 19:41:47 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// const container = document.querySelector(".container");
+// const images = [];
+// const navBar = document.getElementsByClassName("options");
+
+// for (var i = navBar.length - 1; i >= 0; i--) {
+// 	console.log(navBar[i]);
+
+// 	navBar[i].addEventListener("click", function(){
+// 		console.log(navBar[i].classList);
+// 	});
+// }
+
+// for (var i = 9; i >= 1; i--) {
+// 	images[i] = new Image();
+
+// 	images[i].src = "gallery/"+i+".png";
+
+// 	if(images[i].onerror){
+// 		images[i].src = "gallery/"+i+".jpg";
+// 	};
+// 	container.append(images[i]);
+// }
