6d26dd77a7658e805646e971bf01ee97f0473033
Texmod <162580160+Texmod@users.noreply.github.com>
2024-03-08 11:14:28 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,17 @@
 // 	};
 // 	container.append(images[i]);
 // }
+
+const menu = document.querySelector(".menu");
+const list = document.querySelector(".list");
+const dropdown = document.querySelector(".dropdown");
+
+function displaylist(){
+	if(list.style.display == "block"){
+		list.style.display = "none"; 
+		dropdown.style.display = "none"; 
+	}else{
+		list.style.display = "block";
+		dropdown.style.display = "block";  
+	} 
+}
